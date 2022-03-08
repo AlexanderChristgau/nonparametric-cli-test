4f832dca77a55ae85c6edb2e2a7e933f03503e5b
AlexanderChristgau <alex58733@live.dk>
2022-03-08 10:48:17 +0100

    Fixed import of boxhed

--- tst_utils.py
+++ tst_utils.py
@@ -3,18 +3,11 @@ from scipy.linalg import solve
 from sim_utils import *
 from lifelines import CoxTimeVaryingFitter
 
-import os
 import sys
-path = '/Users/bwq666/Documents/GitHub/nonparametric-cli-test/BoXHED2.0'
-sys.path.append(path)
-# import importlib.util
-# spec = importlib.util.spec_from_file_location('boxhed', '/Users/bwq666/Documents/GitHub/nonparametric-cli-test/BoXHED2.0/boxhed.py')
-# module = importlib.util.module_from_spec(spec)
-# sys.modules['boxhed'] = module
-# spec.loader.exec_module(module)
-print('hey', os.path)
-print('j', sys.path[0])
+sys.path.insert(0, './BoXHED2.0') #Cannot use normal relative import due to '.' in directory name
+
 from boxhed import boxhed
+from model_selection import cv
 
 def estimate_Pi(X,Z,L2_pen=0.0001):
     n_s,n_T = Z.shape
